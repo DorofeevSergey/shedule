168781b2c52f0a2aa8190061ed6bee60378de9f5
Sergey Dorofeev <dsi6@mail.msiu.ru>
2011-11-22 19:38:15 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shedule::Application.config.secret_token = 'b5a919cd7b6e60f1286a267f8be97784dbf99d0bbeca482eb880e3e8528c481994ff6c2bf4086f856fc43c9f5fde882d6b7f6a6baa96d7113c23f158133ce7ad'
